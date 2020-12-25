5a370696f6799aa3d73e6bd24ed4f38c29f55775
miner <799022552@qq.com>
2020-12-25 18:15:47 +0800

    httpServer

--- buf_test.js
+++
@@ -1,9 +0,0 @@
-const buf = Buffer.from('runoob', 'ascii');
-
-// 输出 72756e6f6f62
-console.log(buf.toString('hex'));
-
-// 输出 cnVub29i
-console.log(buf.toString('base64'));
-console.log(buf.toString('utf-8'));
-console.log(buf.toString());
--- test/main.js
+++
@@ -1,3 +0,0 @@
-
-var ep = require('./test.js')
-console.log(ep())
